12fbfeb3d0110e2e7770f09fe25a81407c7eb34c
Oleh Krupko <oleh.krupko@hneu.net>
2025-09-25 18:33:44 +0300

    feat(projects): add opportunity to delete projects

--- projects/urls.py
+++ projects/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from projects.views import ProjectListView, ProjectCreateView, ProjectUpdateView
+from projects.views import ProjectListView, ProjectCreateView, ProjectUpdateView, ProjectDeleteView
 
 urlpatterns = [
     path('', ProjectListView.as_view(), name='projects-list'),
     path('add/', ProjectCreateView.as_view(), name='projects-create'),
     path('update/<uuid:pk>/', ProjectUpdateView.as_view(), name='projects-update'),
+path('delete/<uuid:pk>/', ProjectDeleteView.as_view(), name='projects-delete'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.template.loader import render_to_string
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from projects.models import Projects
 
@@ -31,4 +31,18 @@ class ProjectUpdateView(UpdateView):
         self.object = form.save()
         html = render_to_string('partials/projects/project_card.html', {'project': self.object})
         return HttpResponse(html)
+
+
+class ProjectDeleteView(DeleteView):
+    model = Projects
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.delete()
+        # После удаления возвращаем обновлённый список проектов
+        projects = Projects.objects.all()
+        html = render_to_string('partials/projects/projects_list.html', {'projects': projects})
+        return HttpResponse(html)
+
+
 
