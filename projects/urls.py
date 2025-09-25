b22af24f660c01150d2655ee592c90b72ffe5f61
Oleh Krupko <oleh.krupko@hneu.net>
2025-09-25 17:48:00 +0300

    feat(projects): Add ability to edit projects fields

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from projects.views import ProjectListView, ProjectCreateView
+from projects.views import ProjectListView, ProjectCreateView, ProjectUpdateView
 
 urlpatterns = [
     path('', ProjectListView.as_view(), name='projects-list'),
     path('add/', ProjectCreateView.as_view(), name='projects-create'),
+    path('update/<uuid:pk>/', ProjectUpdateView.as_view(), name='projects-update'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.template.loader import render_to_string
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 
 from projects.models import Projects
 
@@ -19,5 +19,16 @@ class ProjectCreateView(CreateView):
     def form_valid(self, form):
         project = form.save()
         html = render_to_string('partials/projects/project_card.html', {'project': project})
+        return HttpResponse(html)
+
+
+class ProjectUpdateView(UpdateView):
+    model = Projects
+    template_name = 'partials/projects/project_update_form.html'
+    fields = '__all__'
+
+    def form_valid(self, form):
+        self.object = form.save()
+        html = render_to_string('partials/projects/project_card.html', {'project': self.object})
         return HttpResponse(html)
 
